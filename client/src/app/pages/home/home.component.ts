a57e2ee14cc53082f665a860e8a507c51e6697eb
mmedeles <mario.medeles@ndus.edu>
2025-04-11 15:10:08 -0500

    Updated testing home page view for mobile feel

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,46 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; // ✅ Import this
-import { ApiService } from '../../services/api.service';
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  imports: [CommonModule],
+  imports: [
+    RouterLink
+  ],
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  status: string = '';
-  timestamp: string = '';
-  counter: number | null = null;
-  counterTimestamp: string = '';
+export class HomeComponent {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor(private apiService: ApiService) {
-  }
-
-  ngOnInit(): void {
-    this.apiService.getBackendStatus().subscribe({
-      next: (res) => {
-        this.status = res.status;
-        this.timestamp = res.timestamp;
-      },
-      error: () => {
-        this.status = 'Failed to fetch backend status';
-      }
-    });
-  }
-
-  increment(): void {
-    this.apiService.incrementCounter().subscribe({
-      next: (res) => {
-        console.log('Backend response:', res); // ✅ Debug
-        this.counter = res.counter;
-        this.counterTimestamp = res.timestamp;
-      },
-      error: (err) => {
-        console.error(err); // ✅ Debug
-        this.counter = -1;
-        this.counterTimestamp = '';
-      }
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: () => this.router.navigate(['/login']),
+      error: () => alert('Logout failed ❌')
     });
   }
 }
