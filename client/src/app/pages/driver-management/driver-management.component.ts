2761dfdecff6667af0335e94b0ee207d866d4507
mmedeles <mario.medeles@ndus.edu>
2025-04-17 15:41:42 -0500

    Added vehicle settings and driver management pages

---
+++ client/src/app/pages/driver-management/driver-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-management',
+  imports: [],
+  templateUrl: './driver-management.component.html',
+  styleUrl: './driver-management.component.scss'
+})
+export class DriverManagementComponent {
+
+}
