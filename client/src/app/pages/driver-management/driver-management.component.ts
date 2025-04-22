028630e43c69bc1421b41a8bbbbcdaf8030e3002
mmedeles <mario.medeles@ndus.edu>
2025-04-21 21:04:43 -0500

    Updated driver management page

--- client/src/app/pages/driver-management/driver-management.component.ts
+++ client/src/app/pages/driver-management/driver-management.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-driver-management',
-  imports: [],
   templateUrl: './driver-management.component.html',
-  styleUrl: './driver-management.component.scss'
+  imports: [
+    FormsModule,
+    NgForOf
+  ],
+  styleUrls: ['./driver-management.component.scss']
 })
 export class DriverManagementComponent {
+  drivers = [
+    { name: 'Franklin Roosevelt', email: 'frank@whitehouse.gov' },
+    { name: 'George Washington', email: 'george@whitehouse.gov' },
+    { name: 'Abraham Lincoln', email: 'abe@whitehouse.gov' }
+  ];
 
+  newDriver = {
+    name: '',
+    email: ''
+  };
+
+  addDriver() {
+    if (this.newDriver.name && this.newDriver.email) {
+      this.drivers.push({ ...this.newDriver });
+      this.newDriver.name = '';
+      this.newDriver.email = '';
+    }
+  }
+
+  deleteDriver(driverToDelete: { name: string, email: string }) {
+    this.drivers = this.drivers.filter(driver => driver !== driverToDelete);
+  }
 }
