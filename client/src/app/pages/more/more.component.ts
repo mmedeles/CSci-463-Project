fa02744660d51a193da32242925126524b296211
mmedeles <mario.medeles@ndus.edu>
2025-04-23 14:59:56 -0500

    Updated More page

--- client/src/app/pages/more/more.component.ts
+++ client/src/app/pages/more/more.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-more',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './more.component.html',
   styleUrl: './more.component.scss'
 })
