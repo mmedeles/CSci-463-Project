d73284661a76c9f4f42dd678671f184782d5a427
mmedeles <mario.medeles@ndus.edu>
2025-04-23 15:16:47 -0500

    Added active section visual to header

--- client/src/app/pages/header/header.component.ts
+++ client/src/app/pages/header/header.component.ts
@@ -1,18 +1,17 @@
 import { Component } from '@angular/core';
-import {Router, RouterLink} from '@angular/router';
+import { Router } from '@angular/router';
 import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   imports: [
-    RouterLink,
     NgIf
   ],
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  isOpen: boolean = false;
+  isOpen = false;
 
   constructor(private router: Router) {}
 
@@ -22,5 +21,21 @@ export class HeaderComponent {
 
   goToProfile(): void {
     this.router.navigate(['/profile']);
+  }
+
+  isActive(path: string): boolean {
+    return this.router.url === path;
+  }
+
+  handleClick(path: string): void {
+    if (this.router.url === path) {
+      // Refresh current route
+      this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigate([path]);
+      });
+    } else {
+      this.router.navigate([path]);
+    }
+    this.isOpen = false;
   }
 }
