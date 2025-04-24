0306ea5aa53f756a83189096f79138aaf2c56026
mmedeles <mario.medeles@ndus.edu>
2025-04-24 15:34:41 -0500

    Updated vehicle metrics page

--- client/src/app/pages/vehicle-metrics/vehicle-metrics.component.ts
+++ client/src/app/pages/vehicle-metrics/vehicle-metrics.component.ts
@@ -16,27 +16,22 @@ export class VehicleMetricsComponent {
     {
       label: 'Engine Temp',
       value: '212°F',
-      icon: 'https://cdn-icons-png.flaticon.com/512/3103/3103982.png'
     },
     {
       label: 'Fuel Economy',
       value: '26.4 MPG',
-      icon: 'https://cdn-icons-png.flaticon.com/512/535/535137.png'
     },
     {
       label: 'Miles Remaining',
       value: '67.2 MI',
-      icon: 'https://cdn-icons-png.flaticon.com/512/2920/2920250.png'
     },
     {
       label: 'Engine RPM',
       value: '2.7K RPM',
-      icon: 'https://cdn-icons-png.flaticon.com/512/7186/7186280.png'
     },
     {
       label: 'External Temp',
       value: '32°F',
-      icon: 'https://cdn-icons-png.flaticon.com/512/869/869869.png'
     }
   ];
 }
