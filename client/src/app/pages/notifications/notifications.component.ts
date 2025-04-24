2b0648e3d09e9257264843636669843febf3c692
mmedeles <mario.medeles@ndus.edu>
2025-04-24 15:23:57 -0500

    Updated notifications page

--- client/src/app/pages/notifications/notifications.component.ts
+++ client/src/app/pages/notifications/notifications.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-notifications',
-  imports: [],
   templateUrl: './notifications.component.html',
-  styleUrl: './notifications.component.scss'
+  imports: [
+    FormsModule,
+    NgForOf
+  ],
+  styleUrls: ['./notifications.component.scss']
 })
-export class NotificationsComponent {
+export class NotificationsComponent implements OnInit {
+  selectedFilter: string = 'All';
+  filterOptions: string[] = ['All', 'Today', 'Yesterday'];
 
+  allNotifications = [
+    {
+      label: 'TODAY',
+      entries: [
+        {
+          title: 'Oil Change Due',
+          time: '2025/4/19 12:40PM',
+          model: '20XX Make Model'
+        },
+        {
+          title: 'Wiper Fluid Empty',
+          time: '2025/4/19 12:35PM',
+          model: '20XX Make Model'
+        }
+      ]
+    },
+    {
+      label: 'YESTERDAY',
+      entries: [
+        {
+          title: 'Tire Pressure Low',
+          time: '2025/4/18 12:40PM',
+          model: '20XX Make Model'
+        }
+      ]
+    }
+  ];
+
+  groupedNotifications = [...this.allNotifications]; // Initially all
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.filterNotifications();
+  }
+
+  filterNotifications(): void {
+    if (this.selectedFilter === 'All') {
+      this.groupedNotifications = [...this.allNotifications];
+    } else {
+      this.groupedNotifications = this.allNotifications.filter(group => group.label === this.selectedFilter.toUpperCase());
+    }
+  }
 }
