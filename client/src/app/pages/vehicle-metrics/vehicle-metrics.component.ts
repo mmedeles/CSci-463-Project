47929e6ef288f32142dbed09c32efa32314a78da
mmedeles <mario.medeles@ndus.edu>
2025-04-24 15:03:47 -0500

    Added vehicle metrics endpoint

---
+++ client/src/app/pages/vehicle-metrics/vehicle-metrics.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-metrics',
+  imports: [],
+  templateUrl: './vehicle-metrics.component.html',
+  styleUrl: './vehicle-metrics.component.scss'
+})
+export class VehicleMetricsComponent {
+
+}
