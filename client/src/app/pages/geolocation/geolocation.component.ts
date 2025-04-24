9bda6565dd932fd6ca8fb598fce570cf9ee0aba9
mmedeles <mario.medeles@ndus.edu>
2025-04-24 15:04:19 -0500

    Added geolocation endpoint

---
+++ client/src/app/pages/geolocation/geolocation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-geolocation',
+  imports: [],
+  templateUrl: './geolocation.component.html',
+  styleUrl: './geolocation.component.scss'
+})
+export class GeolocationComponent {
+
+}
