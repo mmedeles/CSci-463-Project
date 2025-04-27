8a2a7aa30f3bcca1b071c54ad2ad13e7724da18b
mmedeles <mario.medeles@ndus.edu>
2025-04-26 21:17:01 -0500

    Added color priority feature to the notifications page

--- client/src/app/pages/notifications/notifications.component.ts
+++ client/src/app/pages/notifications/notifications.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {NgForOf} from '@angular/common';
-import {HeaderComponent} from '../header/header.component';
+import { FormsModule } from '@angular/forms';
+import {NgClass, NgForOf} from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-notifications',
@@ -9,7 +9,8 @@ import {HeaderComponent} from '../header/header.component';
   imports: [
     FormsModule,
     NgForOf,
-    HeaderComponent
+    HeaderComponent,
+    NgClass
   ],
   styleUrls: ['./notifications.component.scss']
 })
@@ -24,12 +25,14 @@ export class NotificationsComponent implements OnInit {
         {
           title: 'Oil Change Due',
           time: '2025/4/19 12:40PM',
-          model: '20XX Make Model'
+          model: '20XX Make Model',
+          priority: 'warning'
         },
         {
           title: 'Wiper Fluid Empty',
           time: '2025/4/19 12:35PM',
-          model: '20XX Make Model'
+          model: '20XX Make Model',
+          priority: 'informative'
         }
       ]
     },
@@ -39,13 +42,14 @@ export class NotificationsComponent implements OnInit {
         {
           title: 'Tire Pressure Low',
           time: '2025/4/18 12:40PM',
-          model: '20XX Make Model'
+          model: '20XX Make Model',
+          priority: 'critical'
         }
       ]
     }
   ];
 
-  groupedNotifications = [...this.allNotifications]; // Initially all
+  groupedNotifications = [...this.allNotifications];
 
   constructor() {}
 
@@ -58,6 +62,19 @@ export class NotificationsComponent implements OnInit {
       this.groupedNotifications = [...this.allNotifications];
     } else {
       this.groupedNotifications = this.allNotifications.filter(group => group.label === this.selectedFilter.toUpperCase());
+    }
+  }
+
+  getPriorityClass(priority: string): string {
+    switch (priority) {
+      case 'informative':
+        return 'informative';
+      case 'warning':
+        return 'warning';
+      case 'critical':
+        return 'critical';
+      default:
+        return '';
     }
   }
 }
